78370dcb7f24d57790c345f2e8d45226d5ed0cce
Abdul Mughni Wibisono <abdul.mughni@ui.ac.id>
2022-11-24 20:20:02 +0700

    Make login and register

--- sirest/urls.py
+++ sirest/urls.py
@@ -5,4 +5,11 @@ app_name = 'sirest'
 
 urlpatterns = [
     path('', homepage, name = 'homepage'),
+    path('login_register/', login_register, name = 'login_register'),
+    path('login/', login, name = 'login'),
+    path('register/', register, name = 'register'),
+    path('register_admin/', register_admin, name = 'register_admin'),
+    path('register_customer/', register_customer, name = 'register_customer'),
+    path('register_restaurant/', register_restaurant, name = 'register_restaurant'),
+    path('register_courier/', register_courier, name = 'register_courier'),
 ]
--- sirest/views.py
+++ sirest/views.py
@@ -2,3 +2,24 @@ from django.shortcuts import render
 
 def homepage(request):
     return render(request, 'homepage.html')
+
+def login_register(request):
+    return render(request, 'login_register.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'register.html')
+
+def register_admin(request):
+    return render(request, 'register_admin.html')
+
+def register_customer(request):
+    return render(request, 'register_customer.html')
+
+def register_restaurant(request):
+    return render(request, 'register_restaurant.html')
+
+def register_courier(request):
+    return render(request, 'register_courier.html')
