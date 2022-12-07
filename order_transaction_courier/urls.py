e3b50b1a311782d02b1f0c2533614d4a5463d1c0
Malik Ismail <malikismail.0402@gmail.com>
2022-12-07 21:21:54 +0700

    Add changes

--- order_transaction_courier/urls.py
+++ order_transaction_courier/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from .views import *
 
-app_name = 'order_transaction_courier'
+app_name = "order_transaction_courier"
 
 urlpatterns = [
-    path('', read_order_transaction_courier, name = 'r_oder_transaction_courier'),
-    path('summary', summary_order_transaction_courier, name = 'summary_order_transaction_courier')
+    path("", read_order_transaction_courier, name="r_oder_transaction_courier"),
+    path("create", c_order_transaction_courier, name="c_order_transaction_courier"),
 ]
--- order_transaction_courier/views.py
+++ order_transaction_courier/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 
-def read_order_transaction_courier(request):
-    return render(request, 'r_order_transaction_courier.html')
 
-def summary_order_transaction_courier(request):
-    return render(request, 'summary_order_transaction_courier.html')
+def read_order_transaction_courier(request):
+    return render(request, "r_order_transaction_courier.html")
+
+
+def c_order_transaction_courier(request):
+    return render(request, "c_order_transaction_courier.html")
