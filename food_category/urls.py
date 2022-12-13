dcd457a43813557ce48113052e48f017e389741d
Malik Ismail <malikismail.0402@gmail.com>
2022-12-13 18:51:10 +0700

    add food category

--- food_category/urls.py
+++ food_category/urls.py
@@ -6,4 +6,7 @@ app_name = "food_category"
 urlpatterns = [
     path("create", create_food_category, name="create_food_category"),
     path("", read_food_category, name="read_food_category"),
+    path(
+        "delete/<food_category_name>", delete_food_category, name="delete_food_category"
+    ),
 ]
