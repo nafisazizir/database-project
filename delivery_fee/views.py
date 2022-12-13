6b620c27c05645e5aec2013c3f7a0caa6cd81f29
fideraphael <fideraphael@gmail.com>
2022-12-13 23:19:33 +0700

    csrf token and fixing

--- delivery_fee/views.py
+++ delivery_fee/views.py
@@ -9,48 +9,37 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.core import serializers
 from django.db import connection
+from django.views.decorators.csrf import csrf_exempt
 
 def show_fee(request):
     if not request.session.get("isLoggedIn"):
         return redirect('sirest:logout')
     if not request.session.get("role") == 'admin':
         return redirect('sirest:logout')
-    errors = []
     cursor = connection.cursor()
     cursor.execute("SET search_path to PUBLIC")
     email = request.session.get('user_email')
     cursor.execute("SET search_path to SIREST")
     SQL = f"""
-    SELECT province, motorfee, carfee
-    FROM DELIVERY_FEE_PER_KM
+    SELECT *
+    FROM DELIVERY_FEE_PER_KM;
     """
     cursor.execute(SQL)
     delivery_fee_per_km = cursor.fetchall()
 
-    index = 0
-    delivery_fee_per_km_formatted= []
-    for stuff in delivery_fee_per_km:
-        index += 1
-        delivery_fee_per_km_formatted.append((index, stuff[0], stuff[1], stuff[2]))
-
     context = {
-        'errors': errors,
-        'delivery_fee': delivery_fee_per_km_formatted,
+        'delivery_fee': delivery_fee_per_km,
     }
 
     return render(request, "R_delivery_fee.html", context)
 
+@csrf_exempt
 def add_fee(request):
     if not request.session.get("isLoggedIn"):
         return redirect('sirest:logout')
     if not request.session.get("role") == 'admin':
         return redirect('sirest:logout')
 
-    def varcharRandomizer():
-        characters = string.ascii_letters + string.digits
-        varchar = ''.join(random.choice(characters) for i in range(random.randint(6,9)))
-        return varchar
-    errors = []
     cursor = connection.cursor()
     cursor.execute("SET search_path to SIREST")
     
@@ -59,39 +48,36 @@ def add_fee(request):
         motorate= request.POST.get('motorate')
         carrate = request.POST.get('carrate')
         SQL = f"""
-        SELECT id
-        FROM DELIVERY_FEE_PER_KM
+        SELECT MAX(CAST(Id AS NUMERIC))
+        FROM DELIVERY_FEE_PER_KM;
         """
 
         cursor.execute(SQL)
-        id_tuple = [i[0].strip() for i in cursor.fetchall()]
+        id = cursor.fetchone()
 
-        id = varcharRandomizer()
-
-        while id in id_tuple:
-            id = varcharRandomizer()
+        newid = 1
+        if id[0]:
+            newid = int(id[0]) + 1
 
         if province and motorate and carrate:
             SQL = f"""
             INSERT INTO DELIVERY_FEE_PER_KM
             VALUES 
-            ('{id}', '{province}', '{motorate}', '{carrate}')
+            ('{newid}', '{province}', '{motorate}', '{carrate}');
             """
             cursor.execute(SQL)
             return redirect('delivery_fee:show_fee')
 
-        else:
-            errors.append("Please fill every field")
 
-    return render(request, "C_delivery_fee.html", {'errors': errors})
+    return render(request, "C_delivery_fee.html",)
 
+@csrf_exempt
 def change_fee(request, province, motorfee, carfee):
     if not request.session.get("isLoggedIn"):
         return redirect('sirest:logout')
     if not request.session.get("role") == 'admin':
         return redirect('sirest:logout')
 
-    errors = []
     cursor = connection.cursor()
     cursor.execute("SET search_path to PUBLIC")
     email = request.session.get('user_email')
@@ -100,36 +86,26 @@ def change_fee(request, province, motorfee, carfee):
     if request.method == "POST":
         motorate = request.POST.get('motorate')
         carrate = request.POST.get('carrate')
-
         if motorate and carrate:
             SQL = f"""
-            SELECT *
-            FROM DELIVERY_FEE_PER_KM
-            WHERE province = '{province}' AND motorfee = '{motorfee}' AND carfee = '{carfee}'
+            UPDATE DELIVERY_FEE_PER_KM
+            SET motorfee = '{motorate}', carfee = '{carrate}'
+            WHERE id = '{id}' AND province = '{province}' AND motorfee = '{motorfee}' AND carfee = '{carfee}';
             """
-            cursor.execute(SQL)
+        cursor.execute(SQL)
+        return redirect('delivery_fee:show_fee')
 
-            id = cursor.fetchone()[0]
-            print(id)
+    SQL = f"""
+    SELECT id
+    FROM DELIVERY_FEE_PER_KM
+    WHERE province = '{province}' AND motorfee = '{motorfee}' AND carfee = '{carfee}';
+    """
+    cursor.execute(SQL)
 
-            try:
-                SQL = f"""
-                UPDATE DELIVERY_FEE_PER_KM
-                SET motorfee = '{motorate}', carfee = '{carrate}'
-                WHERE id = '{id}' AND province = '{province}' AND motorfee = '{motorfee}' AND carfee = '{carfee}'
-                """
-                cursor.execute(SQL)
-
-                return redirect('delivery_fee:show_fee')
-
-            except:
-                errors.append("Chosen fee does not exist")
+    id = cursor.fetchone()[0]
+    print(id)
         
-        else:
-            errors.append("Please fill all the fields")
-
     context = {
-        'errors': errors,
         'province': province,
         'motorfee': motorfee,
         'carfee': carfee,
@@ -148,7 +124,7 @@ def delete_fee(request, province, motorfee, carfee):
 
     SQL = f"""
         DELETE FROM DELIVERY_FEE_PER_KM
-        WHERE province = '{province}' AND motorfee = '{motorfee}' AND carfee = '{carfee}'
+        WHERE province = '{province}' AND motorfee = '{motorfee}' AND carfee = '{carfee}';
         """
     cursor.execute(SQL)
 
