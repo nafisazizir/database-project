73704e35eb7d3e33b3eb3d6231532e50f7b763c3
Nafis Azizi Riza <101693218+nafisazizir@users.noreply.github.com>
2022-12-13 22:17:14 +0700

    fix procfile and settings.py

--- project_django/settings.py
+++ project_django/settings.py
@@ -29,6 +29,7 @@ SECRET_KEY = "django-insecure-3@5wx%(^*zl68l(o$^m-3%cvjv0g&mom1ra=oj5f048_al57s7
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = [f'https://database-project-ki1.up.railway.app/']
 
 # Application definition
 
@@ -155,7 +156,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
